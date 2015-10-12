a8b3829c7d0b43faa07e3916ac8a8be52f880a82
Nick <handnf@gmail.com>
2015-10-11 23:36:28 -0400

    Added the appropriate endpoints and states. Not tested.

--- edison/main.js
+++ edison/main.js
@@ -1,5 +1,7 @@
 var mraa = require('mraa'); //require mraa
 console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+var https = require('https');
+var API_ENDPOINT = 'http://iotdoorbell.herokuapp.com/api/v1';
 
 //PIN CLASS
 function Pin(pin, direction, value) {
@@ -95,6 +97,7 @@ var cycling_dir_g = -1;
 var button_pressed = false;
 var STATES = {IDLE: 0, CYCLING: 1, USER_BUSY: 2, USER_AVAILABLE: 3, USER_OPEN: 4};
 var state = STATES.IDLE;
+var eventId; //This is the id of the doorbell ring
 setInterval(function() {
     switch(state) {
         case STATES.IDLE:
@@ -102,17 +105,7 @@ setInterval(function() {
             var g = pins.get("green");
             y.write(0.1);
             g.write(0.1);
-            /*y.write(y.getValue()+cycling_dir_y*0.005);
-            if(y.getValue() >= 0.5 || y.getValue() <= 0.1)
-                cycling_dir_y *= -1;
-            if(y.getValue() >= 0.5) {
-                y.write(0.5);
-            } 
-            if(g.getValue() >= 0.5)
-                g.write(0.5);
-            g.write(g.getValue()+cycling_dir_g*0.005);
-            if(g.getValue() >= 0.5 || g.getValue() <= 0.1)
-                cycling_dir_g *= -1;*/
+
             //Check button
             if(pins.get("doorbell").read() == 1 && !button_pressed) {
                 button_pressed = true;
@@ -121,6 +114,7 @@ setInterval(function() {
                 //DO STUFF HUR
                 console.log("Button released");
                 state = STATES.CYCLING;
+                ringDoorbell();
             }
             break;
         case STATES.CYCLING:       
@@ -143,8 +137,88 @@ setInterval(function() {
                 state = STATES.IDLE;
             }
             break;
-        
+        case STATES.USER_AVAILABLE:
+            var y = pins.get("yellow");
+            y.write(0);
+            var g = pins.get("green");
+            g.write(g.getValue()+cycling_dir_g*0.01);
+            if(g.getValue() >= 1 || g.getValue() <= 0)
+                cycling_dir_g *= -1;
+            //Check button to possibly cancel teh call
+            if(pins.get("doorbell").read() == 1 && !button_pressed) {
+                button_pressed = true;
+            } else if(pins.get("doorbell").read() == 0 && button_pressed) {
+                button_pressed = false;
+                //DO STUFF HUR
+                console.log("Button released");
+                state = STATES.IDLE;
+            }
+            break;
+        case STATES.USER_OPEN:
+            var y = pins.get("yellow");
+            y.write(0);
+            var g = pins.get("green");
+            g.write(1);
+            //Check button to possibly cancel teh call
+            if(pins.get("doorbell").read() == 1 && !button_pressed) {
+                button_pressed = true;
+            } else if(pins.get("doorbell").read() == 0 && button_pressed) {
+                button_pressed = false;
+                //DO STUFF HUR
+                console.log("Button released");
+                state = STATES.IDLE;
+            }
+            break;
+        case STATES.USER_BUSY:
+            var g = pins.get("green");
+            g.write(0);
+            var y = pins.get("yellow");
+            y.write(1);
+            //Check button to possibly cancel teh call
+            if(pins.get("doorbell").read() == 1 && !button_pressed) {
+                button_pressed = true;
+            } else if(pins.get("doorbell").read() == 0 && button_pressed) {
+                button_pressed = false;
+                //DO STUFF HUR
+                console.log("Button released");
+                state = STATES.IDLE;
+            }
+            break;
     }
-    
 }, 10);
+/**
+    JSON RESPONSE
+    ?event: event id
+    {result: 2,3,4, time: ms of request}
+**/ 
+setInterval(function() {
+    if(state == STATES.CYCLING) {
+        //Ping the server once/second until there is a response to the current request   
+        https.get(ENDPOINT+"/event?event="+eventId, function(res) {
+            res.on('data', function(d) {
+                var r = JSON.parse(d);
+                if(r.result == STATES.USER_AVAILABLE) 
+                    state = STATES.USER_AVAILABLE;
+                else if(r.result == STATES.USER_BUSY)
+                    state = STATES.USER_BUSY;
+                else if(r.result == STATES.USER_OPEN)
+                    state = STATES.USER_OPEN;
+            });
+        }).on('error', function(e) {
+          console.log("Got error: " + e.message);
+        });
+    }
+}, 1000);
+
+//This function is called when a doorbell is rung. It pings the server to get a request id.
+function ringDoorbell() {
+    https.get(ENDPOINT+"/new", function(res) {
+        res.on('data', function(d) {
+            r = JSON.parse(d); 
+            eventId = d.event_id;
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+}
                
